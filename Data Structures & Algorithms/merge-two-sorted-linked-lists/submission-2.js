9c4e40af0a916e25f37845b076a1e16f5d49d803
Anirudh Bhat <56544816+anirudhjl@users.noreply.github.com>
2026-04-11 10:56:56 +0530

    Add: merge-two-sorted-linked-lists - submission-2

---
+++ Data Structures & Algorithms/merge-two-sorted-linked-lists/submission-2.js
@@ -0,0 +1,12 @@
+class Solution {
+    mergeTwoLists = (l1, l2) => {
+        if (!l1) return l2;
+        if (!l2) return l1;
+        if (l1.val < l2.val) {
+            l1.next = this.mergeTwoLists(l1.next, l2);
+            return l1;
+        }
+        l2.next = this.mergeTwoLists(l1, l2.next);
+        return l2;
+    }
+}
